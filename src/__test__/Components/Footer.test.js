364ffbea015ab5619f5d08341e608e757a1edf18
saniag081 <sgonzalezhernandez081@gmail.com>
2021-08-05 22:36:18 -0500

    test on component Footer

---
+++ src/__test__/Components/Footer.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Footer from '../../components/Footer';
+
+describe('<Footer />', () => {
+  const footer = mount(<Footer />);
+  test('render del componente footer', () => {
+    expect(footer.length).toEqual(1);
+  });
+  test('render del titulo', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
