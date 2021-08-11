51ed585d512354a74d31c73b7f93df72ede6900c
saniag081 <sgonzalezhernandez081@gmail.com>
2021-08-10 21:50:39 -0500

    test on component Header

---
+++ src/__test__/Components/Header.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/providerMock';
+import Header from '../../components/Header';
+
+describe('<Header />', () => {
+  test('render component', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.length).toEqual(1);
+  });
+  test('render del titulo', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
